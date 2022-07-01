cfd47ffa8fcc3be2a79cfc0e73731428ed47ab53
trytodupe <liuandi@live.com>
2022-07-01 22:47:05 +0800

    = fixed version

--- src/main/java/com/trytodupe/ttdaddons/TtdAddons.java
+++ src/main/java/com/trytodupe/ttdaddons/TtdAddons.java
@@ -21,7 +21,7 @@ import java.io.File;
 public class TtdAddons
 {
     public static final String MODID = "ttdaddons";
-    public static final String VERSION = "0.0.12";
+    public static final String VERSION = "0.1.0";
     private static boolean debug = false;
     public static File configurationFile;
     public static final Minecraft mc = Minecraft.getMinecraft();
