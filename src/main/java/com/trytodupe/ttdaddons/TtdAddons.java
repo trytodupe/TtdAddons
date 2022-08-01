be9ee7c71bdf14b3ecd8ae2286a3a6d9f79dc141
trytodupe <liuandi@live.com>
2022-08-01 20:21:52 +0800

    = v0.1.10 fixed gradle (again)

--- src/main/java/com/trytodupe/ttdaddons/TtdAddons.java
+++ src/main/java/com/trytodupe/ttdaddons/TtdAddons.java
@@ -28,7 +28,7 @@ import java.io.File;
 public class TtdAddons
 {
     public static final String MODID = "ttdaddons";
-    public static final String VERSION = "0.1.9";
+    public static final String VERSION = "0.1.10";
     private static boolean debug = false;
     public static File configurationFile;
     public static final Minecraft mc = Minecraft.getMinecraft();
